3f8b9378d38430db4979b73a5e871c4a055c48cf
JaeKwan Ahn <52008@naver.com>
2023-06-17 04:09:51 +0900

    Feat: 임시 api 연결

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,8 +1,18 @@
+'use client';
+
+import axios from 'axios';
+
 export const PostPage = () => {
+  const fetch = () => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/todos/1')
+      .then((res) => console.log(res));
+  };
+
   return (
     <div>
       <p>this is post page</p>
-      <button>api call test</button>
+      <button onClick={fetch}>api call test</button>
     </div>
   );
 };
