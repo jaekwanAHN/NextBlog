c2ee73a874e43a37d5c5f1fc1cc83f3a784de4fd
JaeKwan Ahn <52008@naver.com>
2023-06-19 23:46:49 +0900

    Feat: fetch 결과물 반영 테스트

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,18 +1,21 @@
 'use client';
 
 import axios from 'axios';
+import { useState } from 'react';
 
 export const PostPage = () => {
+  const [tempState, setTempState] = useState('noting in here');
   const fetch = () => {
     axios
       .get('https://jsonplaceholder.typicode.com/todos/1')
-      .then((res) => console.log(res));
+      .then((res) => setTempState(res.data.title));
   };
 
   return (
     <div>
       <p>this is post page</p>
       <button onClick={fetch}>api call test</button>
+      <p>{tempState}</p>
     </div>
   );
 };
